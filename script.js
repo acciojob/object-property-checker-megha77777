33aa0bb9ae0ab869f017f4955a69529367bfb3a0
megha77777 <141833704+megha77777@users.noreply.github.com>
2023-12-26 23:44:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,14 @@ const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
   //   write your code here
+	for(item in sampleObject){
+		if(item==key){
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
 }
 
 // Do not change the code below
